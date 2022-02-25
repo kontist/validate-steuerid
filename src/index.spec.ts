f02fc27d1694b4c6d966eb7cc0abd746bf1cdd83
Uğur Gümüşhan <ugur.gumushan@icloud.com>
2022-02-25 18:35:46 +0300

    WIP: Add tax ID generator (#1)
    * WIP: Add tax ID generator
    
    * Set up typescript
    
    * Increase version number
    
    * Add main prop, typings
    
    * Add default eslintrc
    
    * Change generate functions output type to string
    
    * wann-237: Adjusts the generate function and typescript linting issues
    
    Co-authored-by: Dana Alnusair <dana.alnusair@kontist.com>

---
+++ src/index.spec.ts
@@ -0,0 +1,59 @@
+'use strict'
+import { generate, validate, isOccurencesValid } from './index'
+import * as assert from 'assert'
+
+const examples = [
+  { steuerId: '26954371827', expected: true },
+  { steuerId: '86095742719', expected: true },
+  { steuerId: '65929970489', expected: true },
+  { steuerId: '65299970480', expected: false },
+  { steuerId: '26954371820', expected: false }
+]
+
+for (const example of examples) {
+  it('validates a steuerId successfully', () => {
+    assert.strictEqual(validate(example.steuerId), example.expected)
+  })
+}
+
+it('throws an error if steuerId is not a string', () => {
+  assert.throws(() => validate(65299970480), {
+    name: 'TypeError',
+    message: '`steuerId` must be a string'
+  })
+})
+
+it('throws an error if steuerId does not contain 11 digits', () => {
+  assert.throws(() => validate('26954371'), {
+    name: 'TypeError',
+    message: '`steuerId` must contain exactly 11 digits'
+  })
+})
+
+it('throws an error if steuerId contains non numerical characters', () => {
+  assert.throws(() => validate('26954371rfe'), {
+    name: 'TypeError',
+    message: '`steuerId` can not contain non numerical characters'
+  })
+})
+
+describe('Generate function', () => {
+  it('Is able to generate a number without entering into an infinite loop', () => {
+    const generatedTaxID = generate()
+    assert.deepStrictEqual(typeof generatedTaxID, 'string')
+  })
+
+  it('Generates valid tax ids', () => {
+    for (let i = 0; i < 10; i++) {
+      const generatedTaxID = generate()
+      assert.deepStrictEqual(validate(generatedTaxID), true)
+    }
+  })
+})
+
+describe('isOccurencesValid function returs false on obviously false numbers', () => {
+  it('Return false on triplets 10002345671', () => {
+    const result = isOccurencesValid(String(10002345671).split(''))
+    assert.deepStrictEqual(result, false)
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,167 @@
+'use strict'
+
+const areAllNumbers = (arr: number[]) => {
+  return arr.reduce(function (a, b) {
+    return a && typeof b === 'number' && !isNaN(b)
+  }, true);
+}
+
+const getTail = (arr: number[]) => {
+  const lastIndex = arr.length - 1;
+  return lastIndex >= 0 ? arr[lastIndex] : null;
+}
+
+const getNumOccurencesMap = (arr: number[]) => {
+  const map = {};
+  arr.forEach((num: number) => {
+    if (!(num in map)) {
+      map[num] = 1;
+    } else {
+      map[num] += 1;
+    }
+  });
+  return map;
+}
+
+const groupByNumOfOccurrences = (map: Object) => {
+  const result = {};
+  for (const key in map) {
+    const numOfOccurrence = map[key];
+    if (result[numOfOccurrence] === undefined) {
+      result[numOfOccurrence] = [key];
+    } else {
+      result[numOfOccurrence] = [...result[numOfOccurrence], key];
+    }
+  }
+  return result;
+}
+
+// has to have either one double digit OR one triple digit, never both.
+const checkDoubleOrTriple = (groupedByOccurrences = {}) => {
+  let num2or3occurences = 0;
+
+  for(const key in groupedByOccurrences) {
+    const occurrence = parseInt(key);
+
+    // if there is more than 1 2or3 occurrences return false
+    if (occurrence >= 2) {
+      num2or3occurences++;
+    }
+    
+    if (num2or3occurences > 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Can only have number in two consecutive positions, never three.
+const checkConsecutivePositions = (firstTen: number[]) => {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  return !(firstTen.find((item, index, array) => (item === array[index - 1] && item === array[index - 2])));
+}
+
+const getChecksum = (steuerId: number[]) => {
+  // Takes in an array of numbers
+  const length = steuerId.length
+
+  if (length === 0 || length === undefined || isNaN(length)) return null
+
+  const n = 11
+  const m = 10
+  let digit
+  let product = m
+  let sum
+
+  for (let i = 0; i < length; i++) {
+    digit = steuerId[i]
+    sum = (digit + product) % m
+    if (sum === 0) {
+      sum = m
+    }
+    product = (2 * sum) % n
+  }
+
+  const checkDigit = n - product
+
+  return (checkDigit === 10) ? 0 : checkDigit
+}
+
+export function isOccurencesValid (digits) {
+  const groupedByCharacters = getNumOccurencesMap(digits);
+  const groupedByOccurrences = groupByNumOfOccurrences(groupedByCharacters);
+  let validConsecutive = true;
+
+  if('3' in groupedByOccurrences) {
+    validConsecutive = checkConsecutivePositions(digits);
+  }
+  
+  return checkDoubleOrTriple(groupedByOccurrences) && validConsecutive;
+}
+
+export function validate (steuerId) : boolean{
+  if (typeof steuerId !== 'string') {
+    throw new TypeError('`steuerId` must be a string');
+  }
+
+  // Make sure the steuerId is string then split it into an integer array
+  let steuerIdArr = steuerId.split('').map(n => parseInt(n, 10));
+
+  // Check that steuerId has exactly 11 digits and does not start with 0
+  if (steuerId.length !== 11) {
+    throw new TypeError('`steuerId` must contain exactly 11 digits');
+  }
+  if (!areAllNumbers(steuerIdArr)) {
+    throw new TypeError('`steuerId` can not contain non numerical characters');
+  }
+  if (steuerIdArr[0] === 0) {
+    return false;
+  }
+
+  // Arranges the characters and their occurrences for easier checks.
+  const firstTen = steuerIdArr.slice(0, 10);
+  // Checks the validaty of the steuerId
+  const correct = isOccurencesValid(firstTen);
+
+  if (correct) {
+    // Makes sure that the checksum character also matches.
+    const checksum = getChecksum(firstTen);
+    if (checksum === getTail(steuerIdArr)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/*
+The first number is not 0
+In the first 10 digits there is exactly one number double or triple
+If there are 3 same numbers at the position 1 to 10 those double numbers could never be consecutive
+ */
+export function generate () : string {
+  let digits: number[];
+  digits = [];
+  // does not start with a 0
+  const digit = Math.round((Math.random() * 8) + 1);
+  digits.push(digit);
+
+  let candidate: number[];
+  candidate = [];
+
+  while (digits.length < 10) {
+    let isValidDigit = false;
+    let candidateDigit: number;
+    while (!isValidDigit) {
+      candidateDigit = Math.round(Math.random() * 9)
+      candidate = [...digits, candidateDigit]
+      if (isOccurencesValid(candidate)) {
+        isValidDigit = true;
+      }
+    }
+    digits = [...candidate];
+  }
+
+  return digits.join('') + String(getChecksum(digits))
+}
